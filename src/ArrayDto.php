176a81525790409a82c0b7c502021d6bb1b7f6a0
Deyvid Duarte <deyviddc@gmail.com>
2024-10-02 21:31:33 -0300

    feat: adicao de validacao de schema e de tipos escalares

--- src/ArrayDto.php
+++ src/ArrayDto.php
@@ -3,6 +3,7 @@
 namespace DeyvidDuarte;
 
 use Attribute;
+use InvalidArgumentException;
 
 #[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
 class ArrayDto
@@ -13,7 +14,41 @@ class ArrayDto
 
 	public function validateDto(array $propertyValue)
 	{
-		var_dump($propertyValue);
-		var_dump($this->dtoSchema);
+		$this->validateSchemaStructure($propertyValue);
+		$this->validateSchemaType($propertyValue);
+	}
+
+	private function validateSchemaStructure(array $propertyValue): void
+	{
+		$propertyKeys = array_keys($propertyValue);
+		$schemaKeys = array_keys($this->dtoSchema);
+		$schemaIsValid = count(array_intersect($propertyKeys, $schemaKeys)) == count($this->dtoSchema);
+		if (!$schemaIsValid) {
+			throw new InvalidArgumentException(
+				sprintf(
+					'Invalid DTO Schema: Expected structure %s but received %s',
+					json_encode($schemaKeys), json_encode($propertyKeys)
+				)
+			);
+		}
+	}
+
+	private function validateSchemaType(array $propertyValue): void
+	{
+		$isInvalid = false;
+		$dtoSchemaKeys = array_keys($this->dtoSchema);
+		$propertyKeysInvalid = $propertyValue;
+		foreach ($dtoSchemaKeys as $key) {
+			$propertyTypeValue = gettype($propertyValue[$key]);
+			$propertyKeysInvalid[$key] = $propertyTypeValue;
+			if ($propertyTypeValue != $this->dtoSchema[$key]) {
+				$isInvalid = true;
+			}
+		}
+		$isInvalid && throw new InvalidArgumentException(sprintf(
+			'Invalid DTO Schema: Expected structure %s but received %s',
+			json_encode($this->dtoSchema),
+			json_encode($propertyKeysInvalid)
+		));
 	}
 }
--- tests/ArrayDtoTest.php
+++ tests/ArrayDtoTest.php
@@ -7,16 +7,28 @@ use DeyvidDuarte\{ArrayDto, ArrayDtoHandler};
 
 final class ArrayDtoTest extends TestCase
 {
-	#[ArrayDto(['name' => 'string', 'age' => 'numeric'])]
+	#[ArrayDto(['name' => 'string', 'age' => 'integer', 'average' => 'float', 'approved' => 'bool'])]
 	private array $user;
-	private array $teste;
 
     public function test_deve_validar_que_o_array_nao_possui_os_indices_definidos(): void
     {
 		$this->expectException(InvalidArgumentException::class);
-		$this->expectExceptionMessage('Invalid DTO Schema');
+		$this->expectExceptionMessage(
+			'Invalid DTO Schema: Expected structure ["name","age","average","approved"] but received ["nome","idade","media","aprovado"]'
+		);
 
-		$this->user = ['nome' => 'Deyvid Duarte', 'idade' => 29];
+		$this->user = ['nome' => 'Deyvid Duarte', 'idade' => 29, 'media' => 12, 'aprovado' => true];
+		ArrayDtoHandler::validateSchema($this);
+    }
+
+	public function test_deve_validar_que_o_array_nao_esta_com_os_tipos_corretos(): void
+    {
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage(
+			'Invalid DTO Schema: Expected structure {"name":"string","age":"integer","average":"float","approved":"bool"} but received {"name":"integer","age":"string","average":"integer","approved":"string"}'
+		);
+
+		$this->user = ['name' => 29, 'age' => 'teste', 'average' => 12, 'approved' => 'true'];
 		ArrayDtoHandler::validateSchema($this);
     }
 }
